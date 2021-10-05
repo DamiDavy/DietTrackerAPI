edeea798229f6c4e3b6cc13e64c40b5f81b7ec68
OlPeep <evilmall@mail.ru>
2021-10-06 00:18:32 +0300

    added files and changes for deployment to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ccapi.wsgi --log-file -
--- ccapi/settings.py
+++ ccapi/settings.py
@@ -56,6 +56,7 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -129,11 +130,21 @@ USE_L10N = True
 
 USE_TZ = True
 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+# STATIC_URL = '/static/'
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CORS_ORIGIN_WHITELIST = 'http://localhost:9000', 'https://canthin.netlify.app',
 
